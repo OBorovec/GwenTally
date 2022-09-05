b15bd49cabd8dac4b1c5152f02b37f72aff59126
OBorovec <o.borovec@gmail.com>
2022-09-05 23:06:30 +0200

    release stuff

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 ThemeData appTheme = FlexThemeData.dark(
   colors: const FlexSchemeColor(
     primary: Color(0xff57859d),
-    primaryVariant: Color(0xff50a399),
     secondary: Color(0xffff6e48),
-    secondaryVariant: Color(0xffed7f29),
     appBarColor: Color(0xffed7f29),
     error: Color(0xffcf6679),
   ),
@@ -20,7 +18,6 @@ ThemeData appTheme = FlexThemeData.dark(
   tooltipsMatchBackground: true,
   swapColors: false,
   darkIsTrueBlack: false,
-  useSubThemes: true,
   visualDensity: FlexColorScheme.comfortablePlatformDensity,
   // To use playground font, add GoogleFonts package and uncomment:
   // fontFamily: GoogleFonts.notoSans().fontFamily,
@@ -31,7 +28,6 @@ ThemeData appTheme = FlexThemeData.dark(
     bottomNavigationBarElevation: 0,
     bottomNavigationBarOpacity: 0.95,
     navigationBarOpacity: 0.95,
-    navigationBarMutedUnselectedText: true,
     navigationBarMutedUnselectedIcon: true,
     inputDecoratorIsFilled: true,
     inputDecoratorBorderType: FlexInputBorderType.outline,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gwentboard/constants/app_theme.dart';
 import 'package:gwentboard/route_generator.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,6 +31,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Wakelock.enable();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Gwent Board',
       theme: appTheme,
